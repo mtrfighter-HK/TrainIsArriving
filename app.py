22a807c098d5b313cf8ea56b75e90b820ab948e7
mtrfighter-HK <mtr.fighter@gmail.com>
2026-07-18 09:59:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,14 +8,14 @@ app = Flask(__name__)
 TWL_ORDER = ["CEN", "ADM", "TST", "JOR", "YMT", "MOK", "PRE", "SSP", "CSW", "LCK", "MEF", "LAK", "KWF", "KWH", "TWH", "TSW"]
 
 # ====================== Live API ======================
+
 @app.route('/api/live')
 def get_live_trains():
-    # 動態 progress，讓紅點移動
     t = time.time()
     return {
-        "TWL-UP-1": {"line": "TWL", "direction": "UP", "from": "CEN", "to": "TSW", "progress": (t % 30) / 30, "dest": "荃灣"},
-        "TWL-UP-2": {"line": "TWL", "direction": "UP", "from": "ADM", "to": "TSW", "progress": ((t + 10) % 30) / 30, "dest": "荃灣"},
-        "TWL-DOWN-1": {"line": "TWL", "direction": "DOWN", "from": "TSW", "to": "CEN", "progress": ((t + 15) % 30) / 30, "dest": "中環"},
+        "TWL-UP-1": {"line": "TWL", "direction": "UP", "from": "CEN", "to": "TSW", "progress": (t % 40) / 40, "dest": "荃灣"},
+        "TWL-UP-2": {"line": "TWL", "direction": "UP", "from": "ADM", "to": "TSW", "progress": ((t + 13) % 40) / 40, "dest": "荃灣"},
+        "TWL-DOWN-1": {"line": "TWL", "direction": "DOWN", "from": "TSW", "to": "CEN", "progress": ((t + 25) % 40) / 40, "dest": "中環"},
     }
 
 # ====================== 路由 ======================
